dc8efd3a36b293afd05304f95de8f3403a2cf71e
Besi <david.besenyei97@gmail.com>
2021-03-03 17:27:18 +0100

    Demo controllers

---
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/AdminController.kt
@@ -0,0 +1,22 @@
+package dev.besi.smarthome.backend.controllers
+
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["admin"])
+class AdminController {
+
+    @GetMapping(path = ["test"])
+    @Secured("SYS_ADMIN")
+    fun getTest() = "{\"text\": \"Test passed\"}"
+
+    @GetMapping(path = ["{id}"])
+    fun setAdmin(@PathVariable id: String): String {
+        return FirebaseAuth.getInstance().getUser(id).email
+    }
+
+}
