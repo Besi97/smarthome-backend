d96d19396c5dc0d71119dd919186a14dccac86f5
Besi <david.besenyei97@gmail.com>
2021-03-03 12:23:27 +0100

    Add demo controller

---
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/DemoController.kt
@@ -0,0 +1,10 @@
+package dev.besi.smarthome.backend.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DemoController {
+    @GetMapping("/demo")
+    fun getDemo() = "Hello World"
+}
