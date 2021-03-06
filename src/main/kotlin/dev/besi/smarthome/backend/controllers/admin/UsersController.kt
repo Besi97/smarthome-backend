d9961eb78db8dff0e75ba5f37fc50d8615ec9a24
Besi <david.besenyei97@gmail.com>
2021-03-06 16:52:55 +0100

    Restructure admin endpoints

--- src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
@@ -1,4 +1,4 @@
-package dev.besi.smarthome.backend.controllers
+package dev.besi.smarthome.backend.controllers.admin
 
 import com.google.firebase.auth.FirebaseAuth
 import dev.besi.smarthome.backend.config.SecurityConst
@@ -8,26 +8,23 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping(path = ["admin"])
-class AdminController {
+@RequestMapping(path = ["admin/users"])
+@PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
+class UsersController {
 
-    @GetMapping(path = ["users"])
-    @PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
+    @GetMapping
     fun getUsers(): FirebaseUserPageModel =
             FirebaseUserPageModel(FirebaseAuth.getInstance().listUsers(null))
 
-    @GetMapping(path = ["user/{id}"])
-    @PreAuthorize("hasAnyAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
+    @GetMapping(path = ["{id}"])
     fun getUser(@PathVariable id: String): FirebaseUserModel =
             FirebaseUserModel(FirebaseAuth.getInstance().getUser(id))
 
-    @GetMapping(path = ["user/{id}/claims"])
-    @PreAuthorize("hasAnyAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
+    @GetMapping(path = ["{id}/claims"])
     fun getUserClaims(@PathVariable id: String): Map<String, Any> =
             FirebaseAuth.getInstance().getUser(id).customClaims
 
-    @PostMapping(path = ["user/{id}/claims"])
-    @PreAuthorize("hasAnyAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
+    @PostMapping(path = ["{id}/claims"])
     fun postUserClaims(@PathVariable id: String, @RequestBody claims: Map<String, Any>) =
             FirebaseAuth.getInstance().setCustomUserClaims(id, claims)
 
