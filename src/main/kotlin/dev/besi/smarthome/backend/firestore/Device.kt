c2db0267d424d4441ef598e842234e31e8df1b83
Besi <david.besenyei97@gmail.com>
2021-03-08 18:38:04 +0100

    Add owners to devices

--- src/main/kotlin/dev/besi/smarthome/backend/firestore/Device.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/firestore/Device.kt
@@ -5,7 +5,8 @@ import com.google.cloud.firestore.annotation.DocumentId
 open class Device(
         @DocumentId val id: String? = null,
         val name: String? = null,
-        val type: DeviceType? = null
+        val type: DeviceType? = null,
+        val ownerIds: List<String>? = null
 ) {
     enum class DeviceType {
         DHT_22, LED_DRIVER
