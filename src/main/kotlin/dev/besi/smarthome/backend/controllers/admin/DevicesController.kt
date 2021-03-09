f2f572260c2f18a1476d357cf2569647b1f977d0
Besi <david.besenyei97@gmail.com>
2021-03-09 15:42:17 +0100

    Implement admin devices controller
    Implement listing and posting new devices

--- src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/DevicesController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/DevicesController.kt
@@ -1,16 +1,40 @@
 package dev.besi.smarthome.backend.controllers.admin
 
+import com.google.firebase.cloud.FirestoreClient
 import dev.besi.smarthome.backend.config.SecurityConst
+import dev.besi.smarthome.backend.firestore.Device
+import dev.besi.smarthome.backend.firestore.FirestoreConstants.DEVICES_COLLECTION
+import dev.besi.smarthome.backend.model.DeviceModel
+import dev.besi.smarthome.backend.model.DevicesPageModel
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping(path = ["admin/devices"])
 @PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
 class DevicesController {
 
+	companion object {
+		private const val DEVICES_PAGE_SIZE = 100
+	}
 
+	@GetMapping(path = ["{startAfterId}"])
+	fun getDevices(@PathVariable(required = false) startAfterId: String?): DevicesPageModel =
+			DevicesPageModel(
+					FirestoreClient.getFirestore().collection(DEVICES_COLLECTION).let {
+						(if (startAfterId == null) {
+							it
+						} else {
+							it.startAfter(it.document(startAfterId).get().get())
+						}).limit(DEVICES_PAGE_SIZE).get().get().toObjects(Device::class.java)
+					}
+			)
+
+	@PostMapping
+	fun postDevice(@RequestBody deviceModel: DeviceModel): Device =
+		FirestoreClient.getFirestore().collection(DEVICES_COLLECTION).add(Device(deviceModel))
+				.get().get().get().toObject(Device::class.java)
+				?: Device()
 
 }
