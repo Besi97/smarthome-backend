6c52508d801bee4cd21fa11ae9e817a981841807
Besi <david.besenyei97@gmail.com>
2021-03-09 19:28:32 +0100

    Add household listing for user

--- src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
@@ -1,7 +1,10 @@
 package dev.besi.smarthome.backend.controllers.admin
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.cloud.FirestoreClient
 import dev.besi.smarthome.backend.config.SecurityConst
+import dev.besi.smarthome.backend.firestore.FirestoreConstants.USERS_COLLECTION
+import dev.besi.smarthome.backend.firestore.User
 import dev.besi.smarthome.backend.model.FirebaseUserModel
 import dev.besi.smarthome.backend.model.FirebaseUserPageModel
 import org.springframework.security.access.prepost.PreAuthorize
@@ -27,5 +30,10 @@ class UsersController {
     @PostMapping(path = ["{id}/claims"])
     fun postUserClaims(@PathVariable id: String, @RequestBody claims: Map<String, Any>) =
             FirebaseAuth.getInstance().setCustomUserClaims(id, claims)
+
+    @GetMapping(path = ["{id}/households"])
+    fun getUserHouseholds(@PathVariable id: String): List<String> =
+            FirestoreClient.getFirestore().collection(USERS_COLLECTION).document(id).get()
+                    .get().toObject(User::class.java)!!.householdIds!!
 
 }
