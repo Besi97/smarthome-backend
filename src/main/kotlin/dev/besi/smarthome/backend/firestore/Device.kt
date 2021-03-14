9496739a284d791e07ad12df2d1a4ba1b5e0ba8a
Besi <david.besenyei97@gmail.com>
2021-03-14 18:27:11 +0100

    Update Device
    Device is now a data class.
    It can also only belong to only 1 household, thus householdIds list has been changed to householdId string property.

--- src/main/kotlin/dev/besi/smarthome/backend/firestore/Device.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/firestore/Device.kt
@@ -2,11 +2,11 @@ package dev.besi.smarthome.backend.firestore
 
 import com.google.cloud.firestore.annotation.DocumentId
 
-open class Device(
+data class Device(
         @DocumentId val id: String? = null,
         val name: String? = null,
         val type: DeviceType? = null,
-        val householdIds: List<String>? = null
+        val householdId: String? = null
 ) {
     enum class DeviceType {
         DHT_22, LED_DRIVER
