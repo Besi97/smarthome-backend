a1eba66dc0b4f4f386b20b617000a967bffdd361
Besi <david.besenyei97@gmail.com>
2021-03-14 18:25:46 +0100

    Set JSON media type

--- src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/DevicesController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/DevicesController.kt
@@ -8,6 +8,7 @@ import dev.besi.smarthome.backend.model.DeviceModel
 import dev.besi.smarthome.backend.model.DevicesPageModel
 import dev.besi.smarthome.backend.services.DeviceService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -23,7 +24,7 @@ class DevicesController(
 		private const val DEVICES_PAGE_SIZE = 100
 	}
 
-	@GetMapping
+	@GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 	fun getDevices(@RequestParam(required = false) startAfterId: String?): DevicesPageModel =
 			DevicesPageModel(
 					FirestoreClient.getFirestore().collection(DEVICES_COLLECTION).let {
@@ -35,7 +36,7 @@ class DevicesController(
 					}
 			)
 
-	@PostMapping
+	@PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
 	fun postDevice(@RequestBody deviceModel: DeviceModel): Device =
 			deviceService.createDevice(deviceModel) ?: Device()
 
--- src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/admin/UsersController.kt
@@ -7,6 +7,7 @@ import dev.besi.smarthome.backend.firestore.FirestoreConstants.USERS_COLLECTION
 import dev.besi.smarthome.backend.firestore.User
 import dev.besi.smarthome.backend.model.FirebaseUserModel
 import dev.besi.smarthome.backend.model.FirebaseUserPageModel
+import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
@@ -15,25 +16,25 @@ import org.springframework.web.bind.annotation.*
 @PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
 class UsersController {
 
-    @GetMapping
-    fun getUsers(@RequestParam(required = false) nextPageToken: String?): FirebaseUserPageModel =
-            FirebaseUserPageModel(FirebaseAuth.getInstance().listUsers(nextPageToken))
+	@GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun getUsers(@RequestParam(required = false) nextPageToken: String?): FirebaseUserPageModel =
+			FirebaseUserPageModel(FirebaseAuth.getInstance().listUsers(nextPageToken))
 
-    @GetMapping(path = ["{id}"])
-    fun getUser(@PathVariable id: String): FirebaseUserModel =
-            FirebaseUserModel(FirebaseAuth.getInstance().getUser(id))
+	@GetMapping(path = ["{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun getUser(@PathVariable id: String): FirebaseUserModel =
+			FirebaseUserModel(FirebaseAuth.getInstance().getUser(id))
 
-    @GetMapping(path = ["{id}/claims"])
-    fun getUserClaims(@PathVariable id: String): Map<String, Any> =
-            FirebaseAuth.getInstance().getUser(id).customClaims
+	@GetMapping(path = ["{id}/claims"], produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun getUserClaims(@PathVariable id: String): Map<String, Any> =
+			FirebaseAuth.getInstance().getUser(id).customClaims
 
-    @PostMapping(path = ["{id}/claims"])
-    fun postUserClaims(@PathVariable id: String, @RequestBody claims: Map<String, Any>) =
-            FirebaseAuth.getInstance().setCustomUserClaims(id, claims)
+	@PostMapping(path = ["{id}/claims"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun postUserClaims(@PathVariable id: String, @RequestBody claims: Map<String, Any>) =
+			FirebaseAuth.getInstance().setCustomUserClaims(id, claims)
 
-    @GetMapping(path = ["{id}/households"])
-    fun getUserHouseholds(@PathVariable id: String): List<String> =
-            FirestoreClient.getFirestore().collection(USERS_COLLECTION).document(id).get()
-                    .get().toObject(User::class.java)!!.householdIds!!
+	@GetMapping(path = ["{id}/households"], produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun getUserHouseholds(@PathVariable id: String): List<String> =
+			FirestoreClient.getFirestore().collection(USERS_COLLECTION).document(id).get()
+					.get().toObject(User::class.java)!!.householdIds!!
 
 }
