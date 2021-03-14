fe9d2d07b735e2d0446977ded07f07194a19c5be
Besi <david.besenyei97@gmail.com>
2021-03-14 18:27:55 +0100

    Add household collection string

--- src/main/kotlin/dev/besi/smarthome/backend/firestore/FirestoreConstants.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/firestore/FirestoreConstants.kt
@@ -3,4 +3,5 @@ package dev.besi.smarthome.backend.firestore
 object FirestoreConstants {
 	const val DEVICES_COLLECTION = "devices"
 	const val USERS_COLLECTION = "users"
+	const val HOUSEHOLDS_COLLECTION = "households"
 }
