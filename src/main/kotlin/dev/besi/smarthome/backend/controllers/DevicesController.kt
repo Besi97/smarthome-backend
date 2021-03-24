a4cf686a200226711707c606e1fb2a9fe2960a99
Besi <david.besenyei97@gmail.com>
2021-03-24 16:32:52 +0100

    Create device name update endpoint

--- src/main/kotlin/dev/besi/smarthome/backend/controllers/DevicesController.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/controllers/DevicesController.kt
@@ -3,27 +3,25 @@ package dev.besi.smarthome.backend.controllers
 import dev.besi.smarthome.backend.config.SecurityConst
 import dev.besi.smarthome.backend.exception.FailedToFindSuitableIdException
 import dev.besi.smarthome.backend.repository.entities.Device
-import dev.besi.smarthome.backend.model.AdminDevicesControllerPostCreateDeviceRequestModel
+import dev.besi.smarthome.backend.model.DevicesControllerPostCreateDeviceRequestModel
+import dev.besi.smarthome.backend.model.DevicesControllerPutUpdateDeviceNameRequestModel
 import dev.besi.smarthome.backend.services.DeviceService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping(path = ["devices"])
-@PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
 class DevicesController(
 		@Autowired val deviceService: DeviceService
 ) {
 
+	@PreAuthorize("hasAuthority('${SecurityConst.Scope.SYS_ADMIN}')")
 	@PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
-	fun postDevice(@RequestBody device: AdminDevicesControllerPostCreateDeviceRequestModel): Device? =
+	fun postDevice(@RequestBody device: DevicesControllerPostCreateDeviceRequestModel): Device? =
 			try {
 				deviceService.createDeviceWithType(device.type)
 			} catch (e: FailedToFindSuitableIdException) {
@@ -32,5 +30,16 @@ class DevicesController(
 						"Failed to create device: can not find suitable id"
 				)
 			}
+
+	@PutMapping(
+			path = ["{deviceId: [a-zA-Z0-9]{6}}/name"],
+			consumes = [MediaType.APPLICATION_JSON_VALUE],
+			produces = [MediaType.APPLICATION_JSON_VALUE]
+	)
+	fun updateDeviceName(
+			@RequestBody deviceName: DevicesControllerPutUpdateDeviceNameRequestModel,
+			@PathVariable deviceId: String
+	): Device? =
+			deviceService.updateDeviceName(deviceId, deviceName.name)
 
 }
---
+++ src/main/kotlin/dev/besi/smarthome/backend/model/DevicesControllerPutUpdateDeviceNameRequestModel.kt
@@ -0,0 +1,5 @@
+package dev.besi.smarthome.backend.model
+
+data class DevicesControllerPutUpdateDeviceNameRequestModel(
+		val name: String
+)
