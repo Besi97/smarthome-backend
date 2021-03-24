ccf16cf4af2d86df0a45de1e55c6f08c37cc3a9d
Besi <david.besenyei97@gmail.com>
2021-03-24 16:32:36 +0100

    Update name to remove "admin"

--- src/main/kotlin/dev/besi/smarthome/backend/model/DevicesControllerPostCreateDeviceRequestModel.kt
+++ src/main/kotlin/dev/besi/smarthome/backend/model/DevicesControllerPostCreateDeviceRequestModel.kt
@@ -2,6 +2,6 @@ package dev.besi.smarthome.backend.model
 
 import dev.besi.smarthome.backend.repository.entities.Device
 
-data class AdminDevicesControllerPostCreateDeviceRequestModel(
+data class DevicesControllerPostCreateDeviceRequestModel(
 		val type: Device.DeviceType
 )
